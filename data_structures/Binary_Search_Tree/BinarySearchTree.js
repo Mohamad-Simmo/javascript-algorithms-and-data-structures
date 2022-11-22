1c9d956e078aea93ca2f83068e9edaabda3ecca9
Mohammad Semmo <mohammadsemmo81@gmail.com>
2022-11-22 12:50:48 +0200

    Tree Traversal

--- data_structures/Binary_Search_Tree/BinarySearchTree.js
+++ data_structures/Binary_Search_Tree/BinarySearchTree.js
@@ -31,6 +31,22 @@ class BinarySearchTree {
     }
   }
 
+  insertRecursive(value, head = this.root) {
+    if (head === null) {
+      const newNode = new Node(value);
+      return newNode;
+    }
+
+    if (value > head.value) {
+      // go right
+      head.right = this.insertRecursive(value, head.right);
+    } else {
+      head.left = this.insertRecursive(value, head.left);
+    }
+
+    return head;
+  }
+
   contains(value) {
     if (!this.root) return false;
     let curr = this.root;
@@ -45,6 +61,54 @@ class BinarySearchTree {
       }
     }
     return false;
+  }
+
+  breadthFirstSearch() {
+    let node = this.root,
+      data = [],
+      queue = [];
+    queue.push(node);
+    while (queue.length) {
+      node = queue.shift();
+      data.push(node.value);
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+    }
+    return data;
+  }
+
+  depthFirstSearchPreOrder(arr, node = this.root) {
+    if (!node) return null;
+
+    arr.push(node.value);
+
+    this.depthFirstSearchPreOrder(arr, node.left);
+    this.depthFirstSearchPreOrder(arr, node.right);
+
+    return arr;
+  }
+
+  depthFirstSearchPostOrder(arr, node = this.root) {
+    if (!node) return null;
+
+    this.depthFirstSearchPostOrder(arr, node.left);
+    this.depthFirstSearchPostOrder(arr, node.right);
+
+    arr.push(node.value);
+
+    return arr;
+  }
+
+  depthFirstSearchInOrder(arr, node = this.root) {
+    if (!node) return null;
+
+    this.depthFirstSearchInOrder(arr, node.left);
+
+    arr.push(node.value);
+
+    this.depthFirstSearchInOrder(arr, node.right);
+
+    return arr;
   }
 }
 
--- data_structures/Binary_Search_Tree/index.js
+++ data_structures/Binary_Search_Tree/index.js
@@ -2,9 +2,18 @@ const BinarySearchTree = require('./BinarySearchTree');
 
 const bst = new BinarySearchTree();
 bst.insert(10);
-bst.insert(5);
-bst.insert(11);
+bst.insertRecursive(6);
+bst.insertRecursive(8);
+bst.insertRecursive(3);
+bst.insertRecursive(15);
+bst.insertRecursive(20);
 
 console.dir(bst);
 
 console.log(bst.contains(13));
+
+console.log(bst.depthFirstSearchPostOrder([]));
+
+console.log(bst.depthFirstSearchPreOrder([]));
+
+console.log(bst.depthFirstSearchInOrder([]));
